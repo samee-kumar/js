d7adb44c6c1b6bceb3b01ecaece6da883bca3e99
samee-kumar <sameershegal2023adv@gmail.com>
2025-07-22 14:44:17 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,15 @@
+const accountid ="sAAD"
+let emailid ="soeone@gmail.com"
+var password ="3345"
+ accountCity = " jaipur"
+
+emailid = "gadhdsv@gmail.com"
+password = "68128e9"
+accountCity= "bengluru"
+
+
+
+
+
+console.table([accountid,accountCity,password,emailid])
+
