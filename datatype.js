9fee66823cc9c1915aa973111d0e6703f05cf6fb
samee-kumar <sameershegal2023adv@gmail.com>
2025-07-22 15:22:47 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,18 @@
+"use strict"; /*treat all code as new version of js*/
+//alert=(3+3) we use nodejs not browser
+
+
+//number==> 2 to power 53
+//bigint
+//string=""
+
+//boolean= true/false
+//null => standalone value
+// undefine => 
+ // symbol => uniqueness
+
+
+
+//object 
+console.log(typeof undefined);
+
